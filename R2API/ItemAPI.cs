c52b1b0454f233c88cdcd96e8a15f5369d91c40b
yoshisman8 <4214215+yoshisman8@users.noreply.github.com>
2019-04-10 20:57:44 -0400

    [cleanup intensifies]

--- R2API/ItemAPI.cs
+++ R2API/ItemAPI.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Reflection;
-using MonoMod.RuntimeDetour;
+﻿using System.Collections.ObjectModel;
 using RoR2;
-using UnityEngine;
 
 namespace R2API
 {
 	public static class ItemAPI
 	{
-        public static ObservableCollection<ItemDef> ItemDefinitions { get; private set; }
-	    internal static void InitHooks()
-        {
+		public static ObservableCollection<ItemDef> ItemDefinitions { get; private set; }
+		internal static void InitHooks()
+		{
 
-        }
+		}
 	}
 }
