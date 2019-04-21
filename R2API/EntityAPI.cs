21fb6b9495fcc65126c11c0352217c7ade1ee7af
Tristan McPherson <shredder8910@gmail.com>
2019-04-21 17:11:47 -0400

    Add EntityState hook for proper EntityState deserialization from outside assemblies

---
+++ R2API/EntityAPI.cs
@@ -0,0 +1,27 @@
+﻿using EntityStates;
+using MonoMod.RuntimeDetour;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace R2API
+{
+	public static class EntityAPI
+	{
+		public static void InitHooks()
+		{
+			var detour = new NativeDetour(
+				typeof(SerializableEntityStateType).GetMethod("set_stateType", BindingFlags.Public | BindingFlags.Instance),
+				typeof(EntityAPI).GetMethod(nameof(SetStateTypeDetour), BindingFlags.Public | BindingFlags.Static));
+
+			detour.Apply();
+		}
+
+		private static void SetStateTypeDetour(SerializableEntityStateType self, Type value)
+		{
+			var typeName = typeof(SerializableEntityStateType).CGetField("_typeName");
+			typeName.SetValue(self, ((value != null && value.IsSubclassOf(typeof(EntityState))) ? value.AssemblyQualifiedName : ""));
+		}
+	}
+}
