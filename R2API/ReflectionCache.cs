19a556d7cdc401c3053494ff7d4c0e52871897d1
Tristan McPherson <shredder8910@gmail.com>
2019-04-25 19:02:38 -0400

    Fix caching

--- R2API/ReflectionCache.cs
+++ R2API/ReflectionCache.cs
@@ -8,10 +8,10 @@ namespace R2API
 {
 	public static class ReflectionCache
 	{
-		private static readonly ConcurrentDictionary<(Type, string), FieldInfo> fieldCache = new ConcurrentDictionary<(Type, string), FieldInfo>();
-		private static readonly ConcurrentDictionary<(Type, string), MethodInfo> methodCache = new ConcurrentDictionary<(Type, string), MethodInfo>();
-		private static readonly ConcurrentDictionary<(Type, string, Type[]), MethodInfo> overloadedMethodCache = new ConcurrentDictionary<(Type, string, Type[]), MethodInfo>();
-		private static readonly ConcurrentDictionary<(Type, string), PropertyInfo> propertyCache = new ConcurrentDictionary<(Type, string), PropertyInfo>();
+		private static readonly ConcurrentDictionary<(Type T, string Name), FieldInfo> fieldCache = new ConcurrentDictionary<(Type, string), FieldInfo>();
+		private static readonly ConcurrentDictionary<(Type T, string Name), MethodInfo> methodCache = new ConcurrentDictionary<(Type, string), MethodInfo>();
+		private static readonly ConcurrentDictionary<(Type T, string Name, Type[]), MethodInfo> overloadedMethodCache = new ConcurrentDictionary<(Type, string, Type[]), MethodInfo>();
+		private static readonly ConcurrentDictionary<(Type T, string Name), PropertyInfo> propertyCache = new ConcurrentDictionary<(Type, string), PropertyInfo>();
 
 		private const BindingFlags _bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic;
 
@@ -22,7 +22,7 @@ namespace R2API
 
 		public static FieldInfo GetFieldCached(this Type T, string name, BindingFlags bindingFlags = _bindingFlags)
 		{
-			return fieldCache.GetOrAdd((T, name), T.GetField(name, bindingFlags));
+			return fieldCache.GetOrAdd((T, name), (x) => x.T.GetField(x.Name, bindingFlags));
 		}
 
 		public static MethodInfo GetMethodCached<T>(string name, BindingFlags bindingFlags = _bindingFlags)
@@ -32,12 +32,12 @@ namespace R2API
 
 		public static MethodInfo GetMethodCached(this Type T, string name, BindingFlags bindingFlags = _bindingFlags)
 		{
-			return methodCache.GetOrAdd((T, name), T.GetMethod(name, bindingFlags));
+			return methodCache.GetOrAdd((T, name), (x) => x.T.GetMethod(x.Name, bindingFlags));
 		}
 
 		public static MethodInfo GetMethodCached(this Type T, string name, Type[] argumentTypes, BindingFlags bindingFlags = _bindingFlags)
 		{
-			return overloadedMethodCache.GetOrAdd((T, name, argumentTypes), T.GetMethod(name, bindingFlags, null, argumentTypes, null));
+			return overloadedMethodCache.GetOrAdd((T, name, argumentTypes), (x) => x.T.GetMethod(x.Name, bindingFlags, null, argumentTypes, null));
 		}
 
 		public static PropertyInfo GetPropertyCached<T>(string name, BindingFlags bindingFlags = _bindingFlags)
@@ -47,7 +47,7 @@ namespace R2API
 
 		public static PropertyInfo GetPropertyCached(this Type T, string name, BindingFlags bindingFlags = _bindingFlags)
 		{
-			return propertyCache.GetOrAdd((T, name), T.GetProperty(name, bindingFlags));
+			return propertyCache.GetOrAdd((T, name), (x) => x.T.GetProperty(x.Name, bindingFlags));
 		}
 	}
 }
