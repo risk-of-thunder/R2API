4cd76293a04aaad5949e90f1f0bcc0eba4a912ba
iDeath <xiaoxiao921@hotmail.fr>
2019-10-21 22:13:45 +0200

    ResourcesAPI
    - Allow to register assetbundles for mod makers.
    - Hooking on UnityEngine.Resources.Load is being done so that the paths provided are scanned, if an assetbundle path is recognized it'll instead load the asset directly from the assetbundle. This instead of hooking every place manually is much better.
    - Custom path must follow a pattern like this :
    When an assetbundle is added to the api, a modprefix must come with it, for example : `@MyAssetBundle`
    The path to the custom object provided then looks like this : `@MyAssetBundle:Assets/Import/belt/belt.prefab`

---
+++ R2API/IResourcesProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace R2API {
+    public interface IResourcesProvider {
+        string ModPrefix { get; }
+
+        Object Load(string path, Type type);
+
+        ResourceRequest LoadAsync(string path, Type type);
+
+        Object[] LoadAll(Type type);
+    }
+}
