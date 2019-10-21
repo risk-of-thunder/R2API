c124455c1387249793850b4ab830f5cd0a67a5ed
iDeath <xiaoxiao921@hotmail.fr>
2019-10-21 22:04:46 +0200

    add more to assembly blacklist & modhelper fix
    - Adding more assemblies to ignore in the SearchableAttribute assembly blacklist.
    - CatalogModHelper fix as a monomod patch since runtime generic class hooking is undefined behaviour

---
+++ R2API.MonoMod/CatalogModHelperFix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using MonoMod;
+// ReSharper disable InconsistentNaming
+#pragma warning disable 649
+
+namespace RoR2 {
+    [MonoModPatch("RoR2.CatalogModHelper`1")]
+    class CatalogModHelper<TEntry>{
+
+        public event Action<List<TEntry>> getAdditionalEntries;
+        private readonly Action<int, TEntry> registrationDelegate;
+        private readonly Func<TEntry, string> nameGetter;
+
+        public void CollectAndRegisterAdditionalEntries(ref TEntry[] entries) {
+            int vanillaEntriesLength = entries.Length;
+            List<TEntry> moddedEntries = new List<TEntry>();
+            getAdditionalEntries?.Invoke(moddedEntries);
+
+            moddedEntries.Sort((a, b) => StringComparer.Ordinal.Compare(nameGetter(a), nameGetter(b)));
+            Array.Resize(ref entries, vanillaEntriesLength + moddedEntries.Count);
+
+            for (int i = vanillaEntriesLength; i < moddedEntries.Count + vanillaEntriesLength; i++) {
+                registrationDelegate(i, moddedEntries[i - vanillaEntriesLength]);
+            }
+        }
+    }
+}
