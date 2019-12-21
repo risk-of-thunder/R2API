9951f89a457d65613b106327b9958bf7fa24e5fe
Harbingerofme <harbingerofme@gmail.com>
2019-12-21 01:13:36 +0100

    Add privateMessages

---
+++ R2API/Utils/PrivateMessage.cs
@@ -0,0 +1,20 @@
+﻿using RoR2;
+using UnityEngine.Networking;
+
+namespace R2API.Utils {
+    static class PrivateMessage {
+        public static void SendPrivateMessage(string message, NetworkConnection connection) {
+            SendPrivateMessage(new Chat.SimpleChatMessage() { baseToken = "{0}", paramTokens = new[] { message } }, connection);
+        }
+
+        public static void SendPrivateMessage(Chat.ChatMessageBase message, NetworkConnection connection) {
+            NetworkWriter writer = new NetworkWriter();
+            writer.StartMessage((short)59);
+            writer.Write(message.GetTypeIndex());
+            writer.Write((MessageBase)message);
+            writer.FinishMessage();
+            connection.SendWriter(writer, RoR2.Networking.QosChannelIndex.chat.intVal);
+        }
+    }
+
+}
