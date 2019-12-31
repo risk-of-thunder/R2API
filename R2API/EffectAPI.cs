5c4fb597dcf0056aec8f4da195d0164261d6b918
ReinMasamune <reinmasamuneytstuff@gmail.com>
2019-12-31 12:10:59 -0500

    Added a public Loaded property to detect is submodule is loaded

--- R2API/EffectAPI.cs
+++ R2API/EffectAPI.cs
@@ -10,6 +10,16 @@ namespace R2API {
     // ReSharper disable once InconsistentNaming
     [R2APISubmodule]
     public static class EffectAPI {
+        #region Loaded check
+        //Maybe best to set up a base class or interface that does this automatically?
+        public static bool Loaded {
+            get {
+                return IsLoaded;
+            }
+        }
+        private static bool IsLoaded = false;
+        #endregion
+
         /// <summary>
         /// Mimics events found in CatalogModHelpers, can be used to add or sort effects.
         /// </summary>
@@ -44,6 +54,10 @@ namespace R2API {
         /// <param name="effect">The prefab of the effect to be added</param>
         /// <returns>True if the effect was added</returns>
         public static bool AddEffect(GameObject effect) {
+            if( !IsLoaded ) {
+                R2API.Logger.LogError( "EffectAPI is not loaded. Please use [R2API.Utils.SubModuleDependency]" );
+                return false;
+            }
             if( effect == null ) {
                 Debug.LogError( "Effect prefab was null" );
                 return false;
@@ -78,8 +92,12 @@ namespace R2API {
         /// <param name="effect">The EffectDef to addZ</param>
         /// <returns>False if the EffectDef was null</returns>
         public static bool AddEffect( EffectDef effect ) {
+            if( !IsLoaded ) {
+                R2API.Logger.LogError( "EffectAPI is not loaded. Please use [R2API.Utils.SubModuleDependency]" );
+                return false;
+            }
             if( effect == null ) {
-                Debug.LogError( "EffectDef was null." );
+                R2API.Logger.LogError( "EffectDef was null." );
                 return false;
             }
 
