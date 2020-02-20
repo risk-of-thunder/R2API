db4e6f23653e1b9a56e139d8785a1873f54bbd85
penguin0616 <23770056+penguin0616@users.noreply.github.com>
2020-02-19 22:00:06 -0600

    networkuser -> networkconnection

--- R2API/Utils/DirectMessage.cs
+++ R2API/Utils/DirectMessage.cs
@@ -7,21 +7,48 @@ namespace R2API.Utils {
     /// Class for sending messages directly to singular clients. Mostly useful for Dedicated servers.
     /// </summary>
     public static class DirectMessage {
+        /// <summary>
+		/// Converts NetworkUser to NetworkConnection
+		/// </summary>
+		/// <param name="user"></param>
+		/// <returns>NetworkUser's NetworkConnection</returns>
+		private static UnityEngine.Networking.NetworkConnection ResolveUserToConnection(NetworkUser user) {
+            RoR2.Networking.GameNetworkManager[] networkManagers = (RoR2.Networking.GameNetworkManager[])UnityEngine.Object.FindObjectsOfType(typeof(RoR2.Networking.GameNetworkManager));
+
+            foreach (var networkManager in networkManagers) {
+                UnityEngine.Networking.NetworkConnection networkConnection = networkManager.GetClient(user.GetNetworkPlayerName().steamId);
+
+                if (networkConnection != null) {
+                    return networkConnection;
+                }
+            }
+
+            return null;
+        }
 
         /// <summary>
         /// Sends a string directly to a connection. Useful for when you don't want to take advantage of any preformatted string found in RoR2.Chat.
         /// </summary>
         /// <param name="message">The message to send</param>
-        /// <param name="connection">The network connection to send to. If you can figure out how to convert a networkuser into a NetworkConnection, include it in r2api pls.</param>
+        /// <param name="connection">The network connection to send to.</param>
         public static void SendDirectMessage(string message, NetworkConnection connection) {
             SendDirectMessage(new Chat.SimpleChatMessage() { baseToken = "{0}", paramTokens = new[] { message } }, connection);
+        }
+
+        /// <summary>
+        /// Sends a string directly to a user. Useful for when you don't want to take advantage of any preformatted string found in RoR2.Chat.
+        /// </summary>
+        /// <param name="message">The message to send</param>
+        /// <param name="user">The network user to send to.</param>
+        public static void SendDirectMessage(string message, RoR2.NetworkUser user) {
+            SendDirectMessage(message, ResolveUserToConnection(user));
         }
 
         /// <summary>
         /// Sends a ChatMessage directly to a connection. Checkout RoR2.Chat for possible chatmessage types.
         /// </summary>
         /// <param name="message">The message to send</param>
-        /// <param name="connection">The network connection to send to. If you can figure out how to convert a networkuser into a NetworkConnection, include it in r2api pls.</param>
+        /// <param name="connection">The network connection to send to.</param>
         public static void SendDirectMessage(Chat.ChatMessageBase message, NetworkConnection connection) {
             NetworkWriter writer = new NetworkWriter();
             writer.StartMessage((short)59);
