d60b35be482f9be4a0e07fe30f7c1ba852d7335b
Tristan McPherson <shredder8910@gmail.com>
2020-04-23 13:07:30 -0400

    Update assemblies and GameBuild numbers

--- R2API/LobbyConfigAPI.cs
+++ R2API/LobbyConfigAPI.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 namespace R2API {
     // ReSharper disable once InconsistentNaming
-    [R2APISubmodule(Build = 4233443)]
+    [R2APISubmodule(Build = 4892828)]
     public static class LobbyConfigAPI {
 
         private static int _ruleNameSequence;
