c1495f71815da94992cae0e971b5339eda14e5fc
Death <xiaoxiao921@hotmail.fr>
2020-06-26 23:10:36 +0200

    NetworkingAPI

---
+++ R2API/Networking/Interfaces/INetCommand.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.Networking;
+
+namespace R2API.Networking.Interfaces {
+    public interface INetCommand {
+        void OnReceived();
+    }
+
+    public static class NetCommandExtensions {
+        public static void Send(this INetCommand command, NetworkDestination destination) {
+            if (destination.ShouldRun()) {
+                command.OnReceived();
+            }
+
+            if (destination.ShouldSend()) {
+                var header = destination.GetHeader(NetworkingAPI.GetNetworkHash(command.GetType()));
+
+                if (NetworkServer.active) {
+                    for (int i = 0; i < NetworkServer.connections.Count; ++i) {
+                        NetworkConnection conn = NetworkServer.connections[i];
+                        if (conn == null) {
+                            continue;
+                        }
+
+                        using (Writer netWriter = NetworkingAPI.GetWriter(NetworkingAPI.CommandIndex, conn, QosType.Reliable)) {
+                            NetworkWriter writer = netWriter;
+                            writer.Write(header);
+                        }
+                    }
+                }
+                
+                if (NetworkClient.active) {
+                    using (var netWriter = NetworkingAPI.GetWriter(NetworkingAPI.CommandIndex, ClientScene.readyConnection, QosType.Reliable)) {
+                        NetworkWriter writer = netWriter;
+                        writer.Write(header);
+                    }
+                }
+            }
+        }
+    }
+}
