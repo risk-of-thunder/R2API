5f02a73ba2b106e4ec0d752832743f678e34bc85
Reinms <reinmasamuneytstuff@gmail.com>
2020-11-23 13:03:09 -0500

    Add internal IL helper extensions (Just one for now)

---
+++ R2API/MiscHelpers/ILCursorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MonoMod.Cil;
+
+namespace R2API.MiscHelpers {
+    internal static class ILCursorExtensions {
+        public static ILCursor EmitDel<TDel>(this ILCursor cursor, TDel func)
+            where TDel : Delegate {
+            cursor.EmitDelegate(func);
+            return cursor;
+        }
+    }
+}
