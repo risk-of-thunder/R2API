c4c3bf82e9d7e56dcf227099db096cdc80425ccb
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-03-31 17:29:31 +0200

    bump gamebuild id

--- R2API/R2API.cs
+++ R2API/R2API.cs
@@ -22,7 +22,7 @@ namespace R2API {
         public const string PluginName = "R2API";
         public const string PluginVersion = "0.0.1";
 
-        private const int GameBuild = 5745074;
+        private const int GameBuild = 6427269;
 
         internal new static ManualLogSource Logger { get; set; }
 
