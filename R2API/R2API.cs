a57505acfd59ad6ff3bbbe3ef80f601bcec56fae
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-04-02 19:46:22 +0200

    targeting current game patch. Revert patcher removal

--- R2API/R2API.cs
+++ R2API/R2API.cs
@@ -40,6 +40,7 @@ namespace R2API {
             ModManager = new DetourModManager();
             AddHookLogging();
             CheckForIncompatibleAssemblies();
+            CheckR2APIPatch();
 
             Environment.SetEnvironmentVariable("MONOMOD_DMD_TYPE", "Cecil");
 
@@ -198,6 +199,26 @@ namespace R2API {
                 return;
 
             Logger.LogBlockError(info);
+        }
+
+        // ReSharper disable once InconsistentNaming
+        private static void CheckR2APIPatch() {
+            // This type is injected by the R2API MonoMod patch with MonoModRules
+            const string searchableAttributeScanFixType = "R2API.SearchableAttributeScanFix";
+            var isHere = typeof(RoR2Application).Assembly.GetType(searchableAttributeScanFixType, false) != null;
+
+            if (!isHere) {
+                var message = new List<string> {
+                    "The patch of R2API seems to be missing",
+                    "Please make sure that a file called:",
+                    "R2API.Patcher.dll",
+                    "is present in the Risk of Rain 2\\BepInEx\\patchers\\ folder",
+                    "If you don't have this folder or the dll, please download BepInEx again from the",
+                    "thunderstore and make sure to follow the installation instructions."
+                };
+                Logger.LogBlockError(message);
+                DirectoryUtilities.LogFolderStructureAsTree(Paths.GameRootPath);
+            }
         }
     }
 }
