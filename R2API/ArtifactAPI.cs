a6742bbfacc40f2f07d71c80ed89cab86679da4f
Quentin E. / iDeath <xiaoxiao921@hotmail.fr>
2021-04-04 03:30:30 +0200

    BuffAPI fix, wrappers for artifacts, projectiles prefabs, itemdef, and equipmentdef (#256)
    * Monomod patch -> cecil patch
    Add Bepinex.monomod.hookgen to assembly black list
    
    * remove comment
    
    * update r2api patch checker
    
    * bump gamebuild id
    
    * update mmhook
    
    * update effect api
    
    * add module type so that we know the patch succeed
    
    * update sln folder name for patcher
    
    * cecil need explicit object type ref for base type
    
    * Did people ever used this ? goodbye
    
    * fix dotapi
    
    * must wait until dotStack fields are filled
    
    * SurvivorAPI update to ContentPack
    
    * now has BodyIndex enum
    
    * fix DirectMessage
    
    * fix dotapi compilation
    
    * fix loadoutapi compilation
    
    * R2APIContentPackProvider
    
    * fix SoundAPI compilation
    
    * dont forget Unsethooks
    
    * readonly
    
    * remove useless warnings
    
    * came back from the dead
    
    * new -> CreateInstance
    
    * forgot name assignement
    
    * coding style
    
    * duplicate using
    
    * coding style
    
    * Fix ItemDropAPI IL Hooks
    
    * fix IsElite
    
    * fix ItemIndex ref
    
    * MatchCallOrCallvirt
    
    * barebone itemapi
    
    * Patcher is not needed anymore
    
    * update libs to latest apiref beta build
    
    * targeting current game patch. Revert patcher removal
    
    * update libs for current steam patch
    
    * fix buffapi
    
    * fix eliteapi
    
    * remove inventoryapi
    
    * fix itemapi
    
    * fix loadoutapi
    
    * create RoR2Content early so that modders can use it early
    
    * R2APIContentPackProvider for current patch
    
    * fix survivorapi
    
    * AddBanksToGame OnLoad signature for current patch
    
    * remove unlockablesapi, not worth the effort for now
    
    * use contentpack for state types
    
    * cleanup and fix effectapi
    
    * Add sound banks after the wwise engine is properly init
    
    * create itemdisplayrule set if needed
    
    * Only display if there were any banks to load
    
    * add wrapper around artifact catalog
    
    * fix wrong list being used on the buff catalog
    
    * itemapi now offer wrapper around itemDef
    
    * add wrapper around projectile catalog

---
+++ R2API/ArtifactAPI.cs
@@ -0,0 +1,111 @@
+﻿using R2API.Utils;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace R2API {
+
+    /// <summary>
+    /// API for adding custom artifact to the game.
+    /// </summary>
+    [R2APISubmodule]
+    public static class ArtifactAPI {
+
+        private static readonly List<ArtifactDef> Artifacts = new List<ArtifactDef>();
+
+        private static bool _artifactCatalogInitialized;
+
+        /// <summary>
+        /// Return true if the submodule is loaded.
+        /// </summary>
+        public static bool Loaded {
+            get => _loaded;
+            internal set => _loaded = value;
+        }
+
+        private static bool _loaded;
+
+        #region ModHelper Events and Hooks
+
+        [R2APISubmoduleInit(Stage = InitStage.SetHooks)]
+        internal static void SetHooks() {
+            R2APIContentPackProvider.WhenContentPackReady += AddArtifactsToGame;
+        }
+
+        [R2APISubmoduleInit(Stage = InitStage.UnsetHooks)]
+        internal static void UnsetHooks() {
+            R2APIContentPackProvider.WhenContentPackReady -= AddArtifactsToGame;
+        }
+
+        private static void AddArtifactsToGame(ContentPack r2apiContentPack) {
+            foreach (var artifact in Artifacts) {
+                R2API.Logger.LogInfo($"Custom Artifact: {artifact.cachedName} added");
+            }
+
+            r2apiContentPack.artifactDefs = Artifacts.ToArray();
+            _artifactCatalogInitialized = true;
+        }
+
+        #endregion ModHelper Events and Hooks
+
+        #region Add Methods
+
+        /// <summary>
+        /// Add a custom artifact to the list of available artifacts.
+        /// If this is called after the ArtifactCatalog inits then this will return false and ignore the custom artifact.
+        /// </summary>
+        /// <param name="artifactDef">The artifactDef to add.</param>
+        /// <returns>true if added, false otherwise</returns>
+        public static bool Add(ArtifactDef? artifactDef) {
+            if (!Loaded) {
+                throw new InvalidOperationException($"{nameof(ArtifactAPI)} is not loaded. Please use [{nameof(R2APISubmoduleDependency)}(nameof({nameof(ArtifactAPI)})]");
+            }
+
+            if (_artifactCatalogInitialized) {
+                R2API.Logger.LogError(
+                    $"Too late ! Tried to add artifact: {artifactDef.cachedName} after the artifact list was created");
+                return false;
+            }
+
+            Artifacts.Add(artifactDef);
+            return true;
+        }
+
+        /// <summary>
+        /// Add a custom artifact to the list of available artifacts.
+        /// If this is called after the ArtifactCatalog inits then this will return false and ignore the custom artifact.
+        /// </summary>
+        /// <returns>true if added, false otherwise</returns>
+        public static bool Add(
+            string name,
+            string descriptionToken, string nameToken,
+            GameObject pickupModelPrefab,
+            Sprite smallIconDeselectedSprite, Sprite smallIconSelectedSprite,
+            UnlockableDef unlockableDef) {
+            if (!Loaded) {
+                throw new InvalidOperationException($"{nameof(ArtifactAPI)} is not loaded. Please use [{nameof(R2APISubmoduleDependency)}(nameof({nameof(ArtifactAPI)})]");
+            }
+
+            if (_artifactCatalogInitialized) {
+                R2API.Logger.LogError(
+                    $"Too late ! Tried to add artifact: {name} after the artifact list was created");
+                return false;
+            }
+
+            var artifactDef = ScriptableObject.CreateInstance<ArtifactDef>();
+            artifactDef.cachedName = name;
+            artifactDef.descriptionToken = descriptionToken;
+            artifactDef.nameToken = nameToken;
+            artifactDef.pickupModelPrefab = pickupModelPrefab;
+            artifactDef.smallIconDeselectedSprite = smallIconDeselectedSprite;
+            artifactDef.smallIconSelectedSprite = smallIconSelectedSprite;
+            artifactDef.unlockableDef = unlockableDef;
+
+            Artifacts.Add(artifactDef);
+            return true;
+        }
+
+        #endregion Add Methods
+    }
+}
