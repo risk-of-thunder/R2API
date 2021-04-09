8a1a254732e953955a13af0bc81a4acdfff9282a
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-04-09 22:54:22 +0200

    ItemDropAPI Rein refactor

---
+++ R2API/MiscHelpers/ILCursorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using MonoMod.Cil;
+
+namespace R2API.MiscHelpers {
+    internal static class ILCursorExtensions {
+        public static ILCursor EmitDel<TDel>(this ILCursor cursor, TDel func)
+            where TDel : Delegate {
+            cursor.EmitDelegate(func);
+            return cursor;
+        }
+    }
+}
