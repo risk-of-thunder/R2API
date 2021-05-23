8e1fc10950f47c8c8864d51785967ec427375d34
Quentin E. / iDeath <xiaoxiao921@hotmail.fr>
2021-05-23 22:29:47 +0200

    Fix and cleanup RecalculateStatsAPI (#290)
    * add helper methods for retrieving property getter and setter
    
    * RecalculateStatsAPI : fix local variable capturing not working properly, fix usage of strings and replace with compile time checking

--- R2API/RecalculateStatsAPI.cs
+++ R2API/RecalculateStatsAPI.cs
@@ -92,54 +92,55 @@ namespace R2API {
         /// </summary>
         public static event StatHookEventHandler GetStatCoefficients;
 
+        private static StatHookEventArgs StatMods;
+
         private static void HookRecalculateStats(ILContext il) {
             ILCursor c = new ILCursor(il);
 
-            StatHookEventArgs statMods = null;
             c.Emit(OpCodes.Ldarg_0);
             c.EmitDelegate<Action<CharacterBody>>((cb) => {
-                statMods = new StatHookEventArgs();
-                GetStatCoefficients?.Invoke(cb, statMods);
+                StatMods = new StatHookEventArgs();
+                GetStatCoefficients?.Invoke(cb, StatMods);
             });
 
-            ModifyHealthStat(c, statMods);
-            ModifyShieldStat(c, statMods);
-            ModifyHealthRegenStat(c, statMods);
-            ModifyMovementSpeedStat(c, statMods);
-            ModifyJumpStat(c, statMods);
-            ModifyDamageStat(c, statMods);
-            ModifyAttackSpeedStat(c, statMods);
-            ModifyCritStat(c, statMods);
-            ModifyArmorStat(c, statMods);
+            ModifyHealthStat(c);
+            ModifyShieldStat(c);
+            ModifyHealthRegenStat(c);
+            ModifyMovementSpeedStat(c);
+            ModifyJumpStat(c);
+            ModifyDamageStat(c);
+            ModifyAttackSpeedStat(c);
+            ModifyCritStat(c);
+            ModifyArmorStat(c);
         }
 
-        private static void ModifyArmorStat(ILCursor c, StatHookEventArgs statMods) {
+        private static void ModifyArmorStat(ILCursor c) {
             c.Index = 0;
 
             bool ILFound = c.TryGotoNext(
-                x => x.MatchLdfld<CharacterBody>("baseArmor")
+                x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.baseArmor))
             ) && c.TryGotoNext(
-                x => x.MatchCallOrCallvirt<CharacterBody>("get_armor")
+                x => x.MatchCallOrCallvirt(typeof(CharacterBody).GetPropertyGetter(nameof(CharacterBody.armor)))
             ) && c.TryGotoNext(MoveType.After,
-                x => x.MatchCallOrCallvirt<CharacterBody>("get_armor")
+                x => x.MatchCallOrCallvirt(typeof(CharacterBody).GetPropertyGetter(nameof(CharacterBody.armor)))
             );
 
             if (ILFound) {
                 c.EmitDelegate<Func<float, float>>((oldArmor) => {
-                    return oldArmor + statMods.armorAdd;
+                    return oldArmor + StatMods.armorAdd;
                 });
             }
         }
 
-        private static void ModifyAttackSpeedStat(ILCursor c, StatHookEventArgs statMods) {
+        private static void ModifyAttackSpeedStat(ILCursor c) {
             c.Index = 0;
 
             int locBaseAttackSpeedIndex = -1;
             int locAttackSpeedMultIndex = -1;
             bool ILFound = c.TryGotoNext(
-                x => x.MatchLdfld<CharacterBody>("baseAttackSpeed"),
+                x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.baseAttackSpeed)),
                 x => x.MatchLdarg(0),
-                x => x.MatchLdfld<CharacterBody>("levelAttackSpeed")
+                x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.levelAttackSpeed))
             ) && c.TryGotoNext(
                 x => x.MatchStloc(out locBaseAttackSpeedIndex)
             ) && c.TryGotoNext(
@@ -150,45 +151,45 @@ namespace R2API {
             );
 
             if (ILFound) {
-                c.GotoPrev(x => x.MatchLdfld<CharacterBody>("baseAttackSpeed"));
+                c.GotoPrev(x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.baseAttackSpeed)));
                 c.GotoNext(x => x.MatchStloc(locBaseAttackSpeedIndex));
                 c.EmitDelegate<Func<float, float>>((origSpeed) => {
-                    return origSpeed + statMods.baseAttackSpeedAdd;
+                    return origSpeed + StatMods.baseAttackSpeedAdd;
                 });
                 c.GotoNext(x => x.MatchStloc(locAttackSpeedMultIndex));
                 c.EmitDelegate<Func<float, float>>((origSpeedMult) => {
-                    return origSpeedMult + statMods.attackSpeedMultAdd;
+                    return origSpeedMult + StatMods.attackSpeedMultAdd;
                 });
             }
         }
 
-        private static void ModifyCritStat(ILCursor c, StatHookEventArgs statMods) {
+        private static void ModifyCritStat(ILCursor c) {
             c.Index = 0;
 
             int locOrigCrit = -1;
             bool ILFound = c.TryGotoNext(
                 x => x.MatchLdarg(0),
                 x => x.MatchLdloc(out locOrigCrit),
-                x => x.MatchCallOrCallvirt<CharacterBody>("set_crit"));
+                x => x.MatchCallOrCallvirt(typeof(CharacterBody).GetPropertySetter(nameof(CharacterBody.crit))));
 
             if (ILFound) {
                 c.Emit(OpCodes.Ldloc, locOrigCrit);
                 c.EmitDelegate<Func<float, float>>((origCrit) => {
-                    return origCrit + statMods.critAdd;
+                    return origCrit + StatMods.critAdd;
                 });
                 c.Emit(OpCodes.Stloc, locOrigCrit);
             }
         }
 
-        private static void ModifyDamageStat(ILCursor c, StatHookEventArgs statMods) {
+        private static void ModifyDamageStat(ILCursor c) {
             c.Index = 0;
 
             int locBaseDamageIndex = -1;
             int locDamageMultIndex = -1;
             bool ILFound = c.TryGotoNext(
-                x => x.MatchLdfld<CharacterBody>("baseDamage"),
+                x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.baseDamage)),
                 x => x.MatchLdarg(0),
-                x => x.MatchLdfld<CharacterBody>("levelDamage")
+                x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.levelDamage))
             ) && c.TryGotoNext(
                 x => x.MatchStloc(out locBaseDamageIndex)
             ) && c.TryGotoNext(
@@ -199,45 +200,45 @@ namespace R2API {
             );
 
             if (ILFound) {
-                c.GotoPrev(x => x.MatchLdfld<CharacterBody>("baseDamage"));
+                c.GotoPrev(x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.baseDamage)));
                 c.GotoNext(x => x.MatchStloc(locBaseDamageIndex));
                 c.EmitDelegate<Func<float, float>>((origDamage) => {
-                    return origDamage + statMods.baseDamageAdd;
+                    return origDamage + StatMods.baseDamageAdd;
                 });
                 c.GotoNext(x => x.MatchStloc(locDamageMultIndex));
                 c.EmitDelegate<Func<float, float>>((origDamageMult) => {
-                    return origDamageMult + statMods.damageMultAdd;
+                    return origDamageMult + StatMods.damageMultAdd;
                 });
             }
         }
 
-        private static void ModifyJumpStat(ILCursor c, StatHookEventArgs statMods) {
+        private static void ModifyJumpStat(ILCursor c) {
             c.Index = 0;
 
             bool ILFound = c.TryGotoNext(MoveType.After,
-                x => x.MatchLdfld<CharacterBody>("baseJumpPower"),
+                x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.baseJumpPower)),
                 x => x.MatchLdarg(0),
-                x => x.MatchLdfld<CharacterBody>("levelJumpPower"),
+                x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.levelJumpPower)),
                 x => x.MatchLdloc(out _),
                 x => x.MatchMul(),
                 x => x.MatchAdd());
 
             if (ILFound) {
                 c.EmitDelegate<Func<float, float>>((origJumpPower) => {
-                    return origJumpPower * (1 + statMods.jumpPowerMultAdd);
+                    return origJumpPower * (1 + StatMods.jumpPowerMultAdd);
                 });
             }
         }
 
-        private static void ModifyHealthStat(ILCursor c, StatHookEventArgs statMods) {
+        private static void ModifyHealthStat(ILCursor c) {
             c.Index = 0;
 
             int locBaseHealthIndex = -1;
             int locHealthMultIndex = -1;
             bool ILFound = c.TryGotoNext(
-                x => x.MatchLdfld<CharacterBody>("baseMaxHealth"),
+                x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.baseMaxHealth)),
                 x => x.MatchLdarg(0),
-                x => x.MatchLdfld<CharacterBody>("levelMaxHealth")
+                x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.levelMaxHealth))
             ) && c.TryGotoNext(
                 x => x.MatchStloc(out locBaseHealthIndex)
             ) && c.TryGotoNext(
@@ -248,45 +249,45 @@ namespace R2API {
             );
 
             if (ILFound) {
-                c.GotoPrev(x => x.MatchLdfld<CharacterBody>("baseMaxHealth"));
+                c.GotoPrev(x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.baseMaxHealth)));
                 c.GotoNext(x => x.MatchStloc(locBaseHealthIndex));
                 c.EmitDelegate<Func<float, float>>((origMaxHealth) => {
-                    return origMaxHealth + statMods.baseHealthAdd;
+                    return origMaxHealth + StatMods.baseHealthAdd;
                 });
                 c.GotoNext(x => x.MatchStloc(locHealthMultIndex));
                 c.EmitDelegate<Func<float, float>>((origHealthMult) => {
-                    return origHealthMult + statMods.healthMultAdd;
+                    return origHealthMult + StatMods.healthMultAdd;
                 });
             }
         }
 
-        private static void ModifyShieldStat(ILCursor c, StatHookEventArgs statMods) {
+        private static void ModifyShieldStat(ILCursor c) {
             c.Index = 0;
 
             int locBaseShieldIndex = -1;
             bool ILFound = c.TryGotoNext(
-                x => x.MatchLdfld<CharacterBody>("baseMaxShield"),
+                x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.baseMaxShield)),
                 x => x.MatchLdarg(0),
-                x => x.MatchLdfld<CharacterBody>("levelMaxShield")
+                x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.levelMaxShield))
             ) && c.TryGotoNext(
                 x => x.MatchStloc(out locBaseShieldIndex)
             );
 
             if (ILFound) {
                 c.EmitDelegate<Func<float, float>>((origBaseShield) => {
-                    return origBaseShield + statMods.baseShieldAdd;
+                    return origBaseShield + StatMods.baseShieldAdd;
                 });
             }
         }
 
-        private static void ModifyHealthRegenStat(ILCursor c, StatHookEventArgs statMods) {
+        private static void ModifyHealthRegenStat(ILCursor c) {
             c.Index = 0;
 
             int locRegenMultIndex = -1;
             int locFinalRegenIndex = -1;
             bool ILFound = c.TryGotoNext(
                 x => x.MatchLdloc(out locFinalRegenIndex),
-                x => x.MatchCallOrCallvirt<CharacterBody>("set_regen")
+                x => x.MatchCallOrCallvirt(typeof(CharacterBody).GetPropertySetter(nameof(CharacterBody.regen)))
             ) && c.TryGotoPrev(
                 x => x.MatchAdd(),
                 x => x.MatchLdloc(out locRegenMultIndex),
@@ -297,26 +298,26 @@ namespace R2API {
             if (ILFound) {
                 c.GotoNext(x => x.MatchLdloc(out locRegenMultIndex));
                 c.EmitDelegate<Func<float>>(() => {
-                    return statMods.baseRegenAdd;
+                    return StatMods.baseRegenAdd;
                 });
                 c.Emit(OpCodes.Add);
                 c.GotoNext(x => x.MatchMul());
                 c.EmitDelegate<Func<float, float>>((origRegenMult) => {
-                    return origRegenMult + statMods.regenMultAdd;
+                    return origRegenMult + StatMods.regenMultAdd;
                 });
             }
         }
 
-        private static void ModifyMovementSpeedStat(ILCursor c, StatHookEventArgs statMods) {
+        private static void ModifyMovementSpeedStat(ILCursor c) {
             c.Index = 0;
 
             int locBaseSpeedIndex = -1;
             int locSpeedMultIndex = -1;
             int locSpeedDivIndex = -1;
             bool ILFound = c.TryGotoNext(
-                x => x.MatchLdfld<CharacterBody>("baseMoveSpeed"),
+                x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.baseMoveSpeed)),
                 x => x.MatchLdarg(0),
-                x => x.MatchLdfld<CharacterBody>("levelMoveSpeed")
+                x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.levelMoveSpeed))
             ) && c.TryGotoNext(
                 x => x.MatchStloc(out locBaseSpeedIndex)
             ) && c.TryGotoNext(
@@ -329,14 +330,14 @@ namespace R2API {
             );
 
             if (ILFound) {
-                c.GotoPrev(x => x.MatchLdfld<CharacterBody>("levelMoveSpeed"));
+                c.GotoPrev(x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.levelMoveSpeed)));
                 c.GotoNext(x => x.MatchStloc(locBaseSpeedIndex));
                 c.EmitDelegate<Func<float, float>>((origBaseMoveSpeed) => {
-                    return origBaseMoveSpeed + statMods.baseMoveSpeedAdd;
+                    return origBaseMoveSpeed + StatMods.baseMoveSpeedAdd;
                 });
                 c.GotoNext(x => x.MatchStloc(locSpeedMultIndex));
                 c.EmitDelegate<Func<float, float>>((origMoveSpeedMult) => {
-                    return origMoveSpeedMult + statMods.moveSpeedMultAdd;
+                    return origMoveSpeedMult + StatMods.moveSpeedMultAdd;
                 });
             }
         }
