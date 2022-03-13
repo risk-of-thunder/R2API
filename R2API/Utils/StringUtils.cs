40f6f9b69bd2226e883b03de11a25e147cbc9014
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2022-03-13 02:06:30 +0100

    ThrowIfStringIsNullOrWhiteSpace string util

---
+++ R2API/Utils/StringUtils.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace R2API.Utils {
+    internal class StringUtils {
+        internal static void ThrowIfStringIsNullOrWhiteSpace(string stringToCheck, string paramName) {
+            if (string.IsNullOrWhiteSpace(stringToCheck)) {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+    }
+}
