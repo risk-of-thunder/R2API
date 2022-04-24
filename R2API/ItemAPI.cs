772d30a15c6700b92b253758f4be48b1fefd503d
Nebby <nebby131999@gmail.com>
2022-04-24 10:15:12 -0400

    Potential fix of backwards compat breakage

--- R2API/ItemAPI.cs
+++ R2API/ItemAPI.cs
@@ -330,45 +330,49 @@ namespace R2API {
             bool hidden,
             bool canRemove,
             UnlockableDef unlockableDef,
-            ItemDisplayRule[]? itemDisplayRules,
-            ItemTierDef itemTierDef = null) {
-
-            ItemDef = ScriptableObject.CreateInstance<ItemDef>();
-            ItemDef.canRemove = canRemove;
-            ItemDef.descriptionToken = descriptionToken;
-            ItemDef.hidden = hidden;
-            ItemDef.loreToken = loreToken;
-            ItemDef.name = name;
-            ItemDef.nameToken = nameToken;
-            ItemDef.pickupIconSprite = pickupIconSprite;
-            ItemDef.pickupModelPrefab = pickupModelPrefab;
-            ItemDef.pickupToken = pickupToken;
-            ItemDef.tags = tags;
-            ItemDef.unlockableDef = unlockableDef;
-
-            //If the tier isnt assigned at runtime, load tier from addressables, this should make it so mods that add items dont break.
-            //We dont want to set the .tier directly, because that'll attempt to load the itemTierDef via the itemTierCatalog, and we cant
-            //Guarantee said catalog has been initialized at that point
-            if (tier != ItemTier.AssignedAtRuntime) {
-                ItemDef._itemTierDef = LoadTierFromAddress(tier);
-                return;
-            }
-            else {
-                //If the itemTier is AssignedAtRuntime, but an itemTierDef is not assigned, default to noTier and warn the user
-                if(!itemTierDef) {
-                    ItemDef._itemTierDef = null;
-                    R2API.Logger.LogWarning($"Trying to create an itemDef ({name}), but the \"tier\" argument is set to {nameof(ItemTier.AssignedAtRuntime)}" +
-                        $"And the argument \"itemTierDef\" is null! Resorting to setting tier to NoTier");
-                }
-                else {
-                    ItemDef._itemTierDef = itemTierDef;
-                }
-            }
-
-            ItemDisplayRules = new ItemDisplayRuleDict(itemDisplayRules);
+            ItemDisplayRule[]? itemDisplayRules) {
+            SetupItem(name, nameToken, descriptionToken, loreToken, pickupToken, pickupIconSprite, pickupModelPrefab, tier, tags, canRemove, hidden, unlockableDef, new ItemDisplayRuleDict(itemDisplayRules));
         }
 
         public CustomItem(string name, string nameToken,
+            string descriptionToken, string loreToken,
+            string pickupToken,
+            Sprite pickupIconSprite, GameObject pickupModelPrefab,
+            ItemTag[] tags, ItemTier tier,
+            bool hidden,
+            bool canRemove,
+            UnlockableDef unlockableDef,
+            ItemDisplayRule[]? itemDisplayRules,
+            ItemTierDef itemTierDef = null) {
+            SetupItem(name, nameToken, descriptionToken, loreToken, pickupToken, pickupIconSprite, pickupModelPrefab, tier, tags, canRemove, hidden, unlockableDef, new ItemDisplayRuleDict(itemDisplayRules), itemTierDef);
+        }
+
+        public CustomItem(string name, string nameToken,
+            string descriptionToken, string loreToken,
+            string pickupToken,
+            Sprite pickupIconSprite, GameObject pickupModelPrefab,
+            ItemTier tier, ItemTag[] tags,
+            bool canRemove,
+            bool hidden,
+            UnlockableDef unlockableDef = null,
+            ItemDisplayRuleDict? itemDisplayRules = null){
+            SetupItem(name, nameToken, descriptionToken, loreToken, pickupToken, pickupIconSprite, pickupModelPrefab, tier, tags, canRemove, hidden, unlockableDef, itemDisplayRules);
+        }
+
+        public CustomItem(string name, string nameToken,
+            string descriptionToken, string loreToken,
+            string pickupToken,
+            Sprite pickupIconSprite, GameObject pickupModelPrefab,
+            ItemTier tier, ItemTag[] tags,
+            bool canRemove,
+            bool hidden,
+            UnlockableDef unlockableDef = null,
+            ItemDisplayRuleDict? itemDisplayRules = null,
+            ItemTierDef itemTierDef = null) {
+            SetupItem(name, nameToken, descriptionToken, loreToken, pickupToken, pickupIconSprite, pickupModelPrefab, tier, tags, canRemove, hidden, unlockableDef, itemDisplayRules, itemTierDef);
+        }
+
+        private void SetupItem(string name, string nameToken,
             string descriptionToken, string loreToken,
             string pickupToken,
             Sprite pickupIconSprite, GameObject pickupModelPrefab,
@@ -396,13 +400,13 @@ namespace R2API {
             //If the tier isnt assigned at runtime, load tier from addressables, this should make it so mods that add items dont break.
             //We dont want to set the .tier directly, because that'll attempt to load the itemTierDef via the itemTierCatalog, and we cant
             //Guarantee said catalog has been initialized at that point
-            if(tier != ItemTier.AssignedAtRuntime) {
+            if (tier != ItemTier.AssignedAtRuntime) {
                 ItemDef._itemTierDef = LoadTierFromAddress(tier);
                 return;
             }
             else {
                 //If the itemTier is AssignedAtRuntime, but an itemTierDef is not assigned, default to noTier and warn the user
-                if(!itemTierDef) {
+                if (!itemTierDef) {
                     ItemDef._itemTierDef = null;
                     R2API.Logger.LogWarning($"Trying to create an itemDef ({name}), but the \"tier\" argument is set to {nameof(ItemTier.AssignedAtRuntime)}" +
                         $"And the argument \"itemTierDef\" is null! Resorting to setting tier to NoTier");
@@ -411,33 +415,24 @@ namespace R2API {
                     ItemDef._itemTierDef = itemTierDef;
                 }
             }
+
         }
-
         private ItemTierDef LoadTierFromAddress(ItemTier itemTierToLoad) {
-            switch(itemTierToLoad) {
-                case ItemTier.Tier1:
-                    return LoadTier("RoR2/Base/Common/Tier1Def.asset");
-                case ItemTier.Tier2:
-                    return LoadTier("RoR2/Base/Common/Tier2Def.asset");
-                case ItemTier.Tier3:
-                    return LoadTier("RoR2/Base/Common/Tier3Def.asset");
-                case ItemTier.Lunar:
-                    return LoadTier("RoR2/Base/Common/LunarTierDef.asset");
-                case ItemTier.Boss:
-                    return LoadTier("RoR2/Base/Common/BossTierDef.asset");
-
+            return itemTierToLoad switch {
+                ItemTier.Tier1 => LoadTier("RoR2/Base/Common/Tier1Def.asset"),
+                ItemTier.Tier2 => LoadTier("RoR2/Base/Common/Tier2Def.asset"),
+                ItemTier.Tier3 => LoadTier("RoR2/Base/Common/Tier3Def.asset"),
+                ItemTier.Lunar => LoadTier("RoR2/Base/Common/LunarTierDef.asset"),
+                ItemTier.Boss => LoadTier("RoR2/Base/Common/BossTierDef.asset"),
                 //Void
-                case ItemTier.VoidTier1:
-                    return LoadTier("RoR2/DLC1/Common/VoidTier1Def.asset");
-                case ItemTier.VoidTier2:
-                    return LoadTier("RoR2/DLC1/Common/VoidTier2Def.asset");
-                case ItemTier.VoidTier3:
-                    return LoadTier("RoR2/DLC1/Common/VoidTier3Def.asset");
-                case ItemTier.VoidBoss:
-                    return LoadTier("RoR2/DLC1/Common/VoidBossDef.asset");
-                default:
-                    return null;
-            }
+                ItemTier.VoidTier1 => LoadTier("RoR2/DLC1/Common/VoidTier1Def.asset"),
+                ItemTier.VoidTier2 => LoadTier("RoR2/DLC1/Common/VoidTier2Def.asset"),
+                ItemTier.VoidTier3 => LoadTier("RoR2/DLC1/Common/VoidTier3Def.asset"),
+                ItemTier.VoidBoss => LoadTier("RoR2/DLC1/Common/VoidBossDef.asset"),
+                ItemTier.NoTier => null,
+                ItemTier.AssignedAtRuntime => null,
+                _ => null,
+            };
         }
 
         private ItemTierDef LoadTier(string address) => Addressables.LoadAssetAsync<ItemTierDef>(address).WaitForCompletion();
