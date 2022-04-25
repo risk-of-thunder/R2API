2ed4c49933ba9b1b0be0d5ac5007a9fdbc2d6a4d
Nebby <78170922+Nebby1999@users.noreply.github.com>
2022-04-25 08:49:47 -0400

    ItemAPI tier fix (#394)
    * ItemAPI tier fix
    
    * Made optional param actually optional, fixed broken comment
    
    * Potential fix of backwards compat breakage

--- R2API/ItemAPI.cs
+++ R2API/ItemAPI.cs
@@ -14,6 +14,7 @@ using UnityEngine;
 using R2API.MiscHelpers;
 using Object = UnityEngine.Object;
 using System.Text;
+using UnityEngine.AddressableAssets;
 
 // ReSharper disable MemberCanBePrivate.Global
 // ReSharper disable ClassNeverInstantiated.Global
@@ -330,22 +331,20 @@ namespace R2API {
             bool canRemove,
             UnlockableDef unlockableDef,
             ItemDisplayRule[]? itemDisplayRules) {
+            SetupItem(name, nameToken, descriptionToken, loreToken, pickupToken, pickupIconSprite, pickupModelPrefab, tier, tags, canRemove, hidden, unlockableDef, new ItemDisplayRuleDict(itemDisplayRules));
+        }
 
-            ItemDef = ScriptableObject.CreateInstance<ItemDef>();
-            ItemDef.canRemove = canRemove;
-            ItemDef.descriptionToken = descriptionToken;
-            ItemDef.hidden = hidden;
-            ItemDef.loreToken = loreToken;
-            ItemDef.name = name;
-            ItemDef.nameToken = nameToken;
-            ItemDef.pickupIconSprite = pickupIconSprite;
-            ItemDef.pickupModelPrefab = pickupModelPrefab;
-            ItemDef.pickupToken = pickupToken;
-            ItemDef.tags = tags;
-            ItemDef.tier = tier;
-            ItemDef.unlockableDef = unlockableDef;
-
-            ItemDisplayRules = new ItemDisplayRuleDict(itemDisplayRules);
+        public CustomItem(string name, string nameToken,
+            string descriptionToken, string loreToken,
+            string pickupToken,
+            Sprite pickupIconSprite, GameObject pickupModelPrefab,
+            ItemTag[] tags, ItemTier tier,
+            bool hidden,
+            bool canRemove,
+            UnlockableDef unlockableDef,
+            ItemDisplayRule[]? itemDisplayRules,
+            ItemTierDef itemTierDef = null) {
+            SetupItem(name, nameToken, descriptionToken, loreToken, pickupToken, pickupIconSprite, pickupModelPrefab, tier, tags, canRemove, hidden, unlockableDef, new ItemDisplayRuleDict(itemDisplayRules), itemTierDef);
         }
 
         public CustomItem(string name, string nameToken,
@@ -356,7 +355,33 @@ namespace R2API {
             bool canRemove,
             bool hidden,
             UnlockableDef unlockableDef = null,
-            ItemDisplayRuleDict? itemDisplayRules = null) {
+            ItemDisplayRuleDict? itemDisplayRules = null){
+            SetupItem(name, nameToken, descriptionToken, loreToken, pickupToken, pickupIconSprite, pickupModelPrefab, tier, tags, canRemove, hidden, unlockableDef, itemDisplayRules);
+        }
+
+        public CustomItem(string name, string nameToken,
+            string descriptionToken, string loreToken,
+            string pickupToken,
+            Sprite pickupIconSprite, GameObject pickupModelPrefab,
+            ItemTier tier, ItemTag[] tags,
+            bool canRemove,
+            bool hidden,
+            UnlockableDef unlockableDef = null,
+            ItemDisplayRuleDict? itemDisplayRules = null,
+            ItemTierDef itemTierDef = null) {
+            SetupItem(name, nameToken, descriptionToken, loreToken, pickupToken, pickupIconSprite, pickupModelPrefab, tier, tags, canRemove, hidden, unlockableDef, itemDisplayRules, itemTierDef);
+        }
+
+        private void SetupItem(string name, string nameToken,
+            string descriptionToken, string loreToken,
+            string pickupToken,
+            Sprite pickupIconSprite, GameObject pickupModelPrefab,
+            ItemTier tier, ItemTag[] tags,
+            bool canRemove,
+            bool hidden,
+            UnlockableDef unlockableDef = null,
+            ItemDisplayRuleDict? itemDisplayRules = null,
+            ItemTierDef itemTierDef = null) {
 
             ItemDef = ScriptableObject.CreateInstance<ItemDef>();
             ItemDef.canRemove = canRemove;
@@ -369,11 +394,48 @@ namespace R2API {
             ItemDef.pickupModelPrefab = pickupModelPrefab;
             ItemDef.pickupToken = pickupToken;
             ItemDef.tags = tags;
-            ItemDef.tier = tier;
             ItemDef.unlockableDef = unlockableDef;
-
             ItemDisplayRules = itemDisplayRules;
+
+            //If the tier isnt assigned at runtime, load tier from addressables, this should make it so mods that add items dont break.
+            //We dont want to set the .tier directly, because that'll attempt to load the itemTierDef via the itemTierCatalog, and we cant
+            //Guarantee said catalog has been initialized at that point
+            if (tier != ItemTier.AssignedAtRuntime) {
+                ItemDef._itemTierDef = LoadTierFromAddress(tier);
+                return;
+            }
+            else {
+                //If the itemTier is AssignedAtRuntime, but an itemTierDef is not assigned, default to noTier and warn the user
+                if (!itemTierDef) {
+                    ItemDef._itemTierDef = null;
+                    R2API.Logger.LogWarning($"Trying to create an itemDef ({name}), but the \"tier\" argument is set to {nameof(ItemTier.AssignedAtRuntime)}" +
+                        $"And the argument \"itemTierDef\" is null! Resorting to setting tier to NoTier");
+                }
+                else {
+                    ItemDef._itemTierDef = itemTierDef;
+                }
+            }
+
         }
+        private ItemTierDef LoadTierFromAddress(ItemTier itemTierToLoad) {
+            return itemTierToLoad switch {
+                ItemTier.Tier1 => LoadTier("RoR2/Base/Common/Tier1Def.asset"),
+                ItemTier.Tier2 => LoadTier("RoR2/Base/Common/Tier2Def.asset"),
+                ItemTier.Tier3 => LoadTier("RoR2/Base/Common/Tier3Def.asset"),
+                ItemTier.Lunar => LoadTier("RoR2/Base/Common/LunarTierDef.asset"),
+                ItemTier.Boss => LoadTier("RoR2/Base/Common/BossTierDef.asset"),
+                //Void
+                ItemTier.VoidTier1 => LoadTier("RoR2/DLC1/Common/VoidTier1Def.asset"),
+                ItemTier.VoidTier2 => LoadTier("RoR2/DLC1/Common/VoidTier2Def.asset"),
+                ItemTier.VoidTier3 => LoadTier("RoR2/DLC1/Common/VoidTier3Def.asset"),
+                ItemTier.VoidBoss => LoadTier("RoR2/DLC1/Common/VoidBossDef.asset"),
+                ItemTier.NoTier => null,
+                ItemTier.AssignedAtRuntime => null,
+                _ => null,
+            };
+        }
+
+        private ItemTierDef LoadTier(string address) => Addressables.LoadAssetAsync<ItemTierDef>(address).WaitForCompletion();
     }
 
     public class CustomEquipment {
