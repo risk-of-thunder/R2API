9cf051a59a9343885703f78027c49a39d917f5a4
JaceDaDorito <66852270+JaceDaDorito@users.noreply.github.com>
2023-06-30 23:02:07 -0400

    Upload start of Stage Submodule

---
+++ R2API.Stages/StagesPlugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace R2API;
+
+[BepInPlugin(StagesAPI.PluginGUID, StagesAPI.PluginName, StagesAPI.PluginVersion)]
+public sealed class StagesPlugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger { get; set; }
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+    }
+
+
+}
