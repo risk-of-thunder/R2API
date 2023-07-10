049c790d0fec34670a91f7c3ec6f81c902c1086e
JaceDaDorito <66852270+JaceDaDorito@users.noreply.github.com>
2023-07-10 00:04:42 -0400

    Bug tested the Stage Registration class
    -Fixed ability to register variants,  it balances the weights properly now.
    -Added public debug print methods that print the stages
    -Renamed RegisterSceneDef to RegisterSceneDefToLoop as I will be adding different Registrations for hidden realm alts (like Void Fields)

--- R2API.Stages/StageRegistration.cs
+++ R2API.Stages/StageRegistration.cs
@@ -9,11 +9,15 @@ using System.Text;
 using UnityEngine.AddressableAssets;
 using R2API.ContentManagement;
 using System.Reflection;
+using BepInEx.Logging;
+using HG.Reflection;
+
+[assembly: HG.Reflection.SearchableAttribute.OptIn]
 
 namespace R2API;
 
 /// <summary>
-/// Class for registering SceneDefs into the loop of stages 1-5. Do not use this class for stages that aren't in the loop.
+/// Class for adding and registering SceneDefs
 /// </summary>
 
 #pragma warning disable CS0436 // Type conflicts with imported type
@@ -32,6 +36,24 @@ public static partial class StageRegistration
     private static Material baseBazaarSeerMaterial;
 
     private static bool _hooksEnabled = false;
+
+    [SystemInitializer(typeof(SceneCatalog))]
+    private static void SystemInit()
+    {
+        StagesPlugin.Logger.LogDebug($"Variant dictionary intializing...");
+
+        foreach (SceneCollection sceneCollection in sceneCollections)
+        {
+            foreach (SceneCollection.SceneEntry sceneEntry in sceneCollection.sceneEntries)
+            {
+                SceneDef sceneDef = sceneEntry.sceneDef;
+                if (privateStageVariantDictionary.ContainsKey(sceneDef.baseSceneName) && privateStageVariantDictionary[sceneDef.baseSceneName].Contains(sceneDef))
+                    continue;
+                AddSceneOrVariant(sceneDef, 1);
+            }
+        }
+        RefreshPublicDictionary();
+    }
 
     #region Hooks
     internal static void SetHooks()
@@ -60,6 +82,10 @@ public static partial class StageRegistration
 
     #endregion
 
+    /// <summary>
+    /// Adds a SceneDef to your Mod's ContentPack
+    /// </summary>
+    /// <param name="sceneDef">The SceneDef to add</param>
     public static void AddSceneDef(SceneDef sceneDef)
     {
         StageRegistration.SetHooks();
@@ -70,6 +96,9 @@ public static partial class StageRegistration
         R2APIContentManager.HandleContentAddition(addingAssembly, sceneDef);
     }
 
+    /// <summary>
+    /// A debug util to print each SceneDef in a loop and their respective Weights. All variants of a locale should add up to 1.
+    /// </summary>
     public static void PrintSceneCollections()
     {
         StageRegistration.SetHooks();
@@ -78,21 +107,76 @@ public static partial class StageRegistration
             StagesPlugin.Logger.LogDebug($"Stage {i}");
             foreach(SceneCollection.SceneEntry sceneEntry in sceneCollections[i - 1].sceneEntries)
             {
-                StagesPlugin.Logger.LogDebug($"{sceneEntry.sceneDef.cachedName}, Weight Minus One: {sceneEntry.weightMinusOne}");
+                StagesPlugin.Logger.LogDebug($"{sceneEntry.sceneDef.cachedName}, baseSceneName: {sceneEntry.sceneDef.baseSceneName}, Weight: {sceneEntry.weightMinusOne + 1}");
             }
         }
     }
-
-    public static void RegisterSceneDef(SceneDef sceneDef)
+    /// <summary>
+    /// A debug util to print each SceneDef in a loop and their respective Weights. All variants of a locale should add up to 1.
+    /// </summary>
+    /// <param name="stageNumber">The stages in that stage position</param>
+    public static void PrintSceneCollections(int stageNumber)
     {
         StageRegistration.SetHooks();
+        StagesPlugin.Logger.LogDebug($"Stage {stageNumber}");
+        foreach (SceneCollection.SceneEntry sceneEntry in sceneCollections[stageNumber - 1].sceneEntries)
+        {
+            StagesPlugin.Logger.LogDebug($"{sceneEntry.sceneDef.cachedName}, baseSceneName: {sceneEntry.sceneDef.baseSceneName}, Weight: {sceneEntry.weightMinusOne + 1}");
+        }
+    }
+
+    /// <summary>
+    /// A debug util to print each Scene variant of a specific locale.
+    /// </summary>
+    /// <param name="key">The baseSceneName (ie. "golemplains")</param>
+    public static void PrintSceneVariants(string key)
+    {
+        if (!stageVariantDictionary.ContainsKey(key))
+        {
+            StagesPlugin.Logger.LogError($"Entry {key} doesn't exist or isn't populated yet in the dictionary.");
+            return;
+        }
+
+        StageRegistration.SetHooks();
+        foreach(SceneDef sceneDef in stageVariantDictionary[key])
+        {
+            StagesPlugin.Logger.LogDebug($"{sceneDef.cachedName}, baseSceneName: {sceneDef.baseSceneName}");
+        }
+    }
+
+    /// <summary>
+    /// Registers the SceneDef into the loop. Any SceneDef registered with the same baseSceneName as another SceneDef will be counted as a variant.
+    /// Variants will have their weights split equally amongst each other. Don't use this method for scenes that aren't part of the loop (Stages 1-5).
+    /// </summary>
+    /// <param name="sceneDef">The SceneDef being registered</param>
+    public static void RegisterSceneDefToLoop(SceneDef sceneDef)
+    {
+        StageRegistration.SetHooks();
+        StagesPlugin.Logger.LogDebug($"RegisterSceneDef called from {Assembly.GetCallingAssembly()}");
+
+        if (privateStageVariantDictionary.ContainsKey(sceneDef.baseSceneName) && privateStageVariantDictionary[sceneDef.baseSceneName].Contains(sceneDef))
+        {
+            StagesPlugin.Logger.LogError($"SceneDef {sceneDef.cachedName} is already registered into the Scene Pool");
+            return;
+        }
+
+        if(sceneDef.stageOrder < 1 || sceneDef.stageOrder > numStageCollections)
+        {
+            StagesPlugin.Logger.LogError($"SceneDef {sceneDef.cachedName} has a stage order not within 1-5. Please use this method only for stages within the loop.");
+            return;
+        }
+
         float weight = 1;
         weight = AddSceneOrVariant(sceneDef, weight);
         AppendSceneCollections(sceneDef, weight);
         RefreshPublicDictionary();
-        PrintSceneCollections();
     }
 
+    /// <summary>
+    /// Returns a material for the bazaar seers.
+    /// </summary>
+    /// <param name="texture">The texture to be put in the material</param>
+    /// <returns></returns>
     public static Material MakeBazaarSeerMaterial(Texture2D texture)
     {
         StageRegistration.SetHooks();
@@ -100,6 +184,12 @@ public static partial class StageRegistration
         bazaarMaterial.mainTexture = texture;
         return bazaarMaterial;
     }
+
+    /// <summary>
+    /// Returns a material for the bazaar seers.
+    /// </summary>
+    /// <param name="sceneDef">The SceneDef to be used to make the material. A preview texture must be in the SceneDef.</param>
+    /// <returns></returns>
     public static Material MakeBazaarSeerMaterial(SceneDef sceneDef)
     {
         StageRegistration.SetHooks();
@@ -117,29 +207,32 @@ public static partial class StageRegistration
     {
         int stageOrderIndex = sceneDef.stageOrder - 1;
 
-        if (privateStageVariantDictionary.ContainsKey(sceneDef.baseSceneNameOverride))
+        if (privateStageVariantDictionary.ContainsKey(sceneDef.baseSceneName))
         {
-            List<SceneDef> variants = privateStageVariantDictionary[sceneDef.baseSceneNameOverride];
-            variants.Add(sceneDef);
-            weight = 1f / variants.Count;
+            privateStageVariantDictionary[sceneDef.baseSceneName].Add(sceneDef);
+            weight = 1f / privateStageVariantDictionary[sceneDef.baseSceneName].Count;
 
-
-            for (int i = 0; i < sceneCollections[stageOrderIndex]._sceneEntries.Length; i++)
-            {
-                SceneCollection.SceneEntry sceneEntry = sceneCollections[stageOrderIndex]._sceneEntries[i];
-                if (sceneEntry.sceneDef.baseSceneNameOverride == sceneDef.baseSceneNameOverride)
-                {
-                    sceneEntry.weightMinusOne = weight;
-                }
-            }
+            EqualizeVariantWeights(sceneDef.baseSceneName, stageOrderIndex, weight);
         }
         else
         {
             List<SceneDef> list = new List<SceneDef>();
             list.Add(sceneDef);
-            privateStageVariantDictionary.Add(sceneDef.baseSceneNameOverride, list);
+            privateStageVariantDictionary.Add(sceneDef.baseSceneName, list);
         }
         return weight;
+    }
+
+    //This method also equalizes all vanilla variants since they are already in the SceneCollection, hence why AppendSceneCollections isn't called on SystemInit.
+    private static void EqualizeVariantWeights(string key, int stageOrderIndex, float weight)
+    {
+        for (int i = 0; i < sceneCollections[stageOrderIndex]._sceneEntries.Length; i++)
+        {
+            if (sceneCollections[stageOrderIndex]._sceneEntries[i].sceneDef.baseSceneName.Equals(key))
+            {
+                sceneCollections[stageOrderIndex]._sceneEntries[i].weightMinusOne = weight - 1;
+            }
+        }
     }
 
     private static void AppendSceneCollections(SceneDef sceneDef, float weight)
@@ -156,17 +249,5 @@ public static partial class StageRegistration
         stageVariantDictionary = new ReadOnlyDictionary<string, List<SceneDef>>(privateStageVariantDictionary);
     }
 
-    [SystemInitializer(typeof(SceneCatalog))]
-    private static void SystemInit()
-    {
-        foreach(SceneCollection sceneCollection in sceneCollections)
-        {
-            foreach(SceneCollection.SceneEntry sceneEntry in sceneCollection.sceneEntries)
-            {
-                AddSceneOrVariant(sceneEntry.sceneDef, 1);
-            }
-        }
-        RefreshPublicDictionary();
-        PrintSceneCollections();
-    }
+    
 }
