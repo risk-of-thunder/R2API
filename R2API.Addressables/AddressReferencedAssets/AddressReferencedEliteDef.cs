d4109de9736bcce617d2f41a7ff03aeb89d18fa2
Nebby <nebby131999@gmail.com>
2023-07-31 12:00:05 -0400

    Implemented Missing operators and constructors

--- R2API.Addressables/AddressReferencedAssets/AddressReferencedEliteDef.cs
+++ R2API.Addressables/AddressReferencedAssets/AddressReferencedEliteDef.cs
@@ -38,4 +38,55 @@ public class AddressReferencedEliteDef : AddressReferencedAsset<EliteDef>
         }
         LoadFromAddress();
     }
+
+    /// <summary>
+    /// Operator for casting <see cref="AddressReferencedEliteDef"/> to a boolean value
+    /// <br>Allows you to keep using the unity Syntax for checking if an object exists.</br>
+    /// </summary>
+    public static implicit operator bool(AddressReferencedEliteDef addressReferencedAsset)
+    {
+        return addressReferencedAsset?.Asset;
+    }
+
+    /// <summary>
+    /// Operator for casting <see cref="AddressReferencedEliteDef"/> to it's currently loaded <see cref="Asset"/> value
+    /// </summary>
+    public static implicit operator EliteDef(AddressReferencedEliteDef addressReferencedAsset)
+    {
+        return addressReferencedAsset?.Asset;
+    }
+
+    /// <summary>
+    /// Operator for encapsulating a <see cref="string"/> inside an <see cref="AddressReferencedEliteDef"/>
+    /// </summary>
+    public static implicit operator AddressReferencedEliteDef(string address)
+    {
+        return new AddressReferencedEliteDef(address);
+    }
+
+    /// <summary>
+    /// Operator for encapsulating an <see cref="EliteDef"/> inside an <see cref="AddressReferencedEliteDef"/>
+    /// </summary>
+    public static implicit operator AddressReferencedEliteDef(EliteDef asset)
+    {
+        return new AddressReferencedEliteDef(asset);
+    }
+
+    /// <summary>
+    /// <inheritdoc cref="AddressReferencedAsset{T}.AddressReferencedAsset()"/>
+    /// <br>T is <see cref="EliteDef"/></br>
+    /// </summary>
+    public AddressReferencedEliteDef() : base() { }
+
+    /// <summary>
+    /// <inheritdoc cref="AddressReferencedAsset{T}.AddressReferencedAsset(T)"/>
+    /// <br>T is <see cref="EliteDef"/></br>
+    /// </summary>
+    public AddressReferencedEliteDef(EliteDef def) : base(def) { }
+
+    /// <summary>
+    /// <inheritdoc cref="AddressReferencedAsset{T}.AddressReferencedAsset(string)"/>
+    /// <br>T is <see cref="EliteDef"/></br>
+    /// </summary>
+    public AddressReferencedEliteDef(string addressOrName) : base(addressOrName) { }
 }
