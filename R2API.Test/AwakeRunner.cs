13ed57ceb763a0df0c40e6bd946c5058bed1b67d
Quentin <837334+xiaoxiao921@users.noreply.github.com>
2024-07-19 20:39:45 +0200

    fix(R2API.Test): fix R2API.Test not being able to be used in the game env due to stupid xUnit non viable netstandard 2.0 dependency. (#524)

--- R2API.Test/AwakeRunner.cs
+++ R2API.Test/AwakeRunner.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Linq;
-using System.Xml.Linq;
-using Xunit;
-using Xunit.Abstractions;
-using Xunit.Runners;
+using System.Reflection;
+using R2API.TestingLibrary;
 
 namespace R2API.Test;
 
@@ -12,109 +10,39 @@ internal class AwakeRunner
     internal void DiscoverAndRun()
     {
         var assembly = typeof(R2APITest).Assembly;
-        var path = assembly.Location;
+        const BindingFlags allFlags = (BindingFlags)(-1);
+        var testMethods = assembly.GetTypes()
+            .SelectMany(t => t.GetMethods(allFlags))
+            .Where(m => m.GetCustomAttributes(typeof(FactAttribute), false).Length > 0)
+            .ToArray();
 
-        R2APITest.Logger.LogInfo($"Discovering tests in {path}...");
-        var assemblyElement = new XElement("assembly");
-        try
+        int passedTests = 0;
+        int failedTests = 0;
+        R2APITest.Logger.LogInfo($"Test Count: {testMethods.Length}");
+        foreach (var method in testMethods)
         {
-            var messageSink = new MessageSink
+            var instance = Activator.CreateInstance(method.DeclaringType);
+            try
             {
-                OnTest = OnTest,
-                OnExecutionComplete = OnExecutionComplete,
-            };
-
-            using (
-                var controller = new XunitFrontController(
-                    AppDomainSupport.Denied,
-                    path
-                )
-            )
+                method.Invoke(instance, null);
+                R2APITest.Logger.LogInfo($"[Tests] [PASS] {method.DeclaringType.Name}.{method.Name}");
+                passedTests++;
+            }
+            catch (Exception ex)
             {
-                var configuration = ConfigReader.Load(path);
-                configuration.AppDomain = AppDomainSupport.IfAvailable;
-                configuration.DiagnosticMessages = true;
-                configuration.StopOnFail = true;
-                configuration.MaxParallelThreads = 1;
-                configuration.LongRunningTestSeconds = 5;
-                ITestFrameworkDiscoveryOptions discoveryOptions =
-                    TestFrameworkOptions.ForDiscovery(configuration);
-                discoveryOptions.SetSynchronousMessageReporting(true);
-                discoveryOptions.SetPreEnumerateTheories(false);
-                controller.Find(false, messageSink, discoveryOptions);
-                messageSink.DiscoveryCompletionWaitHandle.WaitOne();
-                ITestCase[] testCases = messageSink.TestCases.ToArray();
-                lock (this)
-                {
-                    R2APITest.Logger.LogInfo(
-                        $"{testCases.Length} test cases were found in {path}:"
-                    );
-                    foreach (ITestCase testCase in testCases)
-                    {
-                        R2APITest.Logger.LogInfo($"- {testCase.DisplayName}");
-                    }
-
-                    Console.Error.Flush();
-                }
-
-                ITestFrameworkExecutionOptions executionOptions =
-                    TestFrameworkOptions.ForExecution(configuration);
-                executionOptions.SetDiagnosticMessages(true);
-                executionOptions.SetSynchronousMessageReporting(true);
-                executionOptions.SetStopOnTestFail(true);
-                executionOptions.SetDisableParallelization(true);
-
-                controller.RunTests(
-                    testCases,
-                    messageSink,
-                    executionOptions
-                );
-                messageSink.ExecutionCompletionWaitHandle.WaitOne();
+                R2APITest.Logger.LogError($"[Tests] [FAIL] {method.DeclaringType.Name}.{method.Name} - {ex.InnerException?.Message ?? ex.Message}");
+                failedTests++;
             }
         }
-        catch (Exception e)
+        R2APITest.Logger.LogInfo($"Test Count: {passedTests}");
+        R2APITest.Logger.LogInfo($"Passed Tests: {passedTests}");
+        if (failedTests > 0)
         {
-            R2APITest.Logger.LogError($"{e.GetType().Name}: {e.Message}");
-            R2APITest.Logger.LogError(e.StackTrace);
+            R2APITest.Logger.LogError($"Failed Tests: {failedTests}");
         }
-        R2APITest.Logger.LogInfo($"All tests in {path} ran.");
-    }
-
-    private void OnExecutionComplete(ExecutionCompleteInfo info)
-    {
-        R2APITest.Logger.LogInfo($"Total:   {info.TotalTests}");
-        R2APITest.Logger.LogInfo($"Passed:  {info.TotalTests - info.TestsFailed - info.TestsSkipped}");
-        R2APITest.Logger.LogInfo($"Failed:  {info.TestsFailed}");
-        R2APITest.Logger.LogInfo($"Skipped: {info.TestsSkipped}");
-        if (info.TestsFailed > 0)
+        else
         {
-            //ExitCode = 1;
-        }
-    }
-
-    private void OnTest(TestInfo info)
-    {
-        switch (info)
-        {
-            case TestPassedInfo i:
-                R2APITest.Logger.LogInfo($"PASS {i.TestDisplayName}: {i.ExecutionTime}s");
-                return;
-
-            case TestFailedInfo i:
-                R2APITest.Logger.LogError(
-                    $"FAIL {i.TestDisplayName}:" +
-                    $"{i.ExecutionTime}s\n" +
-                    $"{i.ExceptionType}:" +
-                    $"{string.Join("\n  ", i.ExceptionMessage.Split('\n'))}\n" +
-                    $"{string.Join("\n  ", i.ExceptionStackTrace.Split('\n'))}\n\n" +
-                    $"Output:\n" +
-                    $"{i.Output.Replace("\n", "\n  ")}"
-                );
-                return;
-
-            case TestSkippedInfo i:
-                R2APITest.Logger.LogWarning($"SKIP {i.TestDisplayName}: {i.SkipReason}");
-                return;
+            R2APITest.Logger.LogInfo($"Zero Failed Tests");
         }
     }
 }
