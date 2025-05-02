5263765d873934f8b5af84cfb455d1399a312509
Nux <138419122+Nuxlar@users.noreply.github.com>
2025-05-02 16:33:14 -0400

    ContentAddition 1.0.9 GameMode Support (#595)
    * 1.0.9 GameMode Support
    
    Adds improved modded game mode support
    
    * Fixed custom component not working
    
    * bump ver
    
    ---------
    
    Co-authored-by: xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>

---
+++ R2API.ContentManagement/GameModeInfo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace R2API;
+
+/// <summary>
+/// Component to attach to your GameMode prefab. buttonHoverDescription is the only current attribute.
+/// </summary>
+public class GameModeInfo : MonoBehaviour
+{
+    public string buttonHoverDescription;
+}
