d3f1139ea08b4ce1e9358ff1165a4b67e9eb6abe
Brynzananas <brynzananas@gmail.com>
2025-05-25 03:22:05 +0600

    Create SkillDefInterop.cs

---
+++ R2API.Skills.Interop/SkillDefInterop.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using RoR2;
+
+[assembly:InternalsVisibleTo("R2API.Skills")]
+namespace R2API.Skills.Interop;
+
+public static class SkillDefInterop
+{
+    public static int GetBonusStockMultiplier(SkillDef skillDef) => skillDef.r2api_bonusStockMultiplier;
+}
