2bdbf53c899d700c7fd302ccc04dd363673701c5
Brynzananas <brynzananas@gmail.com>
2025-05-25 03:25:48 +0600

    Add setting the bonus

--- R2API.Skills.Interop/SkillDefInterop.cs
+++ R2API.Skills.Interop/SkillDefInterop.cs
@@ -8,4 +8,5 @@ namespace R2API.Skills.Interop;
 public static class SkillDefInterop
 {
     public static int GetBonusStockMultiplier(SkillDef skillDef) => skillDef.r2api_bonusStockMultiplier;
+    public static void SetBonusStockMultiplier(SkillDef skillDef, int value) => skillDef.r2api_bonusStockMultiplier = value;
 }
