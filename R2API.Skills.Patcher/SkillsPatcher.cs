eff9096cf7f332ed3ddd3255d03ca5087247492a
Brynzananas <brynzananas@gmail.com>
2025-05-25 03:19:58 +0600

    actually patch it to skillDef instead of generic skills

--- R2API.Skills.Patcher/SkillsPatcher.cs
+++ R2API.Skills.Patcher/SkillsPatcher.cs
@@ -21,6 +21,7 @@ internal static class SkillsPatcher
         genericSkill?.Fields.Add(new FieldDefinition("r2api_hideInCharacterSelectIfFirstSkillSelected", FieldAttributes.Public, assembly.MainModule.ImportReference(typeof(bool))));
         genericSkill?.Fields.Add(new FieldDefinition("r2api_orderPriority", FieldAttributes.Public, assembly.MainModule.ImportReference(typeof(int))));
         genericSkill?.Fields.Add(new FieldDefinition("r2api_loadoutTitleTokenOverride", FieldAttributes.Public, assembly.MainModule.ImportReference(typeof(string))));
-        genericSkill?.Fields.Add(new FieldDefinition("r2api_bonusStockMultiplier", FieldAttributes.Public, assembly.MainModule.ImportReference(typeof(int))));
+        TypeDefinition skillDef = assembly.MainModule.GetType("RoR2", "SkillDef");
+        skillDef?.Fields.Add(new FieldDefinition("r2api_bonusStockMultiplier", FieldAttributes.Public, assembly.MainModule.ImportReference(typeof(int))));
     }
 }
