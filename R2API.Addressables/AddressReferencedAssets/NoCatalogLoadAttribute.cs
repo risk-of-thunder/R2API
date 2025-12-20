231e895c9a94780e015303095cd8f158ac190721
Nebby <nebby131999@gmail.com>
2025-12-20 16:14:27 -0300

    Add AddressableComponentRequirementAttribute

--- R2API.Addressables/AddressReferencedAssets/NoCatalogLoadAttribute.cs
+++ R2API.Addressables/AddressReferencedAssets/NoCatalogLoadAttribute.cs
@@ -8,9 +8,9 @@ namespace R2API.AddressReferencedAssets;
 /// <summary>
 /// An Attribute which marks a <see cref="AddressReferencedAsset{T}"/> that it should not be loaded via the ingame catalogs.
 /// <para></para>
-/// This attribute does nothing at runtime, it's meant to be a PropertyAttribute for usage in an editor environment alongside RoR2EditorKit.
+/// This attribute does nothing at runtime, it's meant to be a Attribute for usage in an editor environment alongside RoR2EditorKit.
 /// </summary>
-public class NoCatalogLoadAttribute : PropertyAttribute
+public class NoCatalogLoadAttribute : Attribute
 {
 
 }
