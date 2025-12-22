e89de2d3d141dc6d17d0f2e916a8eaceee984e93
Nebby <78170922+Nebby1999@users.noreply.github.com>
2025-12-22 11:04:25 -0300

    R2API.Addressables: Add new Attributes for Addressable usage within the editor (#666)
    * Add NoCatalogLoad attribute
    
    * Add AddressableComponentRequirementAttribute
    
    * Update README.md

---
+++ R2API.Addressables/AddressReferencedAssets/AddressableComponentRequirementAttribute.cs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.AddressableAssets;
+using UnityEngine;
+
+namespace R2API.AddressReferencedAssets;
+
+/// <summary>
+/// An Attribute which can be used to mark a <see cref="AddressReferencedPrefab"/> or a <see cref="AssetReferenceGameObject"/> that it requires the GameObject to have a specific component.
+/// <para></para>
+/// This attribute does nothing at runtime, it's meant to be an Attribute for usage in an editor environment alongside RoR2EditorKit.
+/// </summary>
+public class AddressableComponentRequirementAttribute : Attribute
+{
+    /// <summary>
+    /// The required component that must exist within the addressable GameObject
+    /// </summary>
+    public Type requiredComponentType { get; }
+
+    /// <summary>
+    /// If true, the component will be queried using <see cref="GameObject.GetComponentInChildren(Type)"/> instead of <see cref="GameObject.TryGetComponent(Type, out Component)"/>
+    /// </summary>
+    public bool searchInChildren { get; set; }
+
+    /// <summary>
+    /// Constructor for <see cref="AddressableComponentRequirementAttribute"/>
+    /// </summary>
+    /// <param name="requiredComponentType">The required component that must exist within the addressable GameObject</param>
+#pragma warning disable IDE0290 // Use primary constructor
+    public AddressableComponentRequirementAttribute(Type requiredComponentType)
+#pragma warning restore IDE0290 // Use primary constructor
+    {
+        this.requiredComponentType = requiredComponentType;
+    }
+}
--- R2API.Addressables/README.md
+++ R2API.Addressables/README.md
@@ -8,6 +8,11 @@ Currently it adds the AddressReferencedAsset system, which allows you on the Edi
 
 ## Changelog
 
+### '1.1.3'
+
+* Added the ``NoCataloLoad`` attribute, which when implemented on a field and in conjunction with RoR2EditorKit, forces an AddressReferencedAsset to never be loaded from the catalog.
+* Added the ``AddressableComponentRequirement`` attribute, when used with an AssetReferenceGameObject or an AddressReferencedPrefab, it can restrict the lookup search in RoR2EditorKit to only include Game objects that have the specified component
+
 ### '1.1.2'
 
 * Reimplemented version '1.1.0' with fixes, changes and improvements.
